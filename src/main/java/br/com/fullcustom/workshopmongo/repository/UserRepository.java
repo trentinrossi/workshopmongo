50745c9d6ae5adc4def74669c21b3739838715d9
Rodrigo Rossi <rdrg.rossi@gmail.com>
2020-07-20 13:11:07 -0300

    Obtendo um usuario por id

--- src/main/java/br/com/fullcustom/workshopmongo/repository/UserRepository.java
+++ src/main/java/br/com/fullcustom/workshopmongo/repository/UserRepository.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Repository;
 import br.com.fullcustom.workshopmongo.domain.User;
 
 @Repository
-public interface UserRepository extends MongoRepository<User, Long> {
+public interface UserRepository extends MongoRepository<User, String> {
     
 }
--- src/main/java/br/com/fullcustom/workshopmongo/services/UserService.java
+++ src/main/java/br/com/fullcustom/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package br.com.fullcustom.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.fullcustom.workshopmongo.domain.User;
 import br.com.fullcustom.workshopmongo.repository.UserRepository;
+import br.com.fullcustom.workshopmongo.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,6 +18,11 @@ public class UserService {
 
     public List<User> findAll() {
         return repository.findAll();
+    }
+
+    public User findById(String id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
 
 }
