eb2fe4e763bbe5008ccad6d669cc598dafab50c3
Rodrigo Rossi <rdrg.rossi@gmail.com>
2020-07-20 13:13:24 -0300

    Insercao de usuario com POST

--- src/main/java/br/com/fullcustom/workshopmongo/services/UserService.java
+++ src/main/java/br/com/fullcustom/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.fullcustom.workshopmongo.domain.User;
+import br.com.fullcustom.workshopmongo.dto.UserDTO;
 import br.com.fullcustom.workshopmongo.repository.UserRepository;
 import br.com.fullcustom.workshopmongo.services.exceptions.ObjectNotFoundException;
 
@@ -24,5 +25,13 @@ public class UserService {
         Optional<User> obj = repository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
+
+    public User insert(User obj) {
+		return repository.insert(obj);
+	}
+
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
 
 }
