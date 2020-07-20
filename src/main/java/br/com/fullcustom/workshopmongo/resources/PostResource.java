cbd88eca277d270776cc040bf6247c6bf84ef26f
Rodrigo Rossi <rdrg.rossi@gmail.com>
2020-07-20 15:12:35 -0300

    Obtendo um post por id

---
+++ src/main/java/br/com/fullcustom/workshopmongo/resources/PostResource.java
@@ -0,0 +1,25 @@
+package br.com.fullcustom.workshopmongo.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.fullcustom.workshopmongo.domain.Post;
+import br.com.fullcustom.workshopmongo.services.PostService;
+
+@RestController
+@RequestMapping(value = "/posts")
+public class PostResource {
+
+    @Autowired
+    private PostService service;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Post> findById(@PathVariable String id) {
+        Post obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+}
---
+++ src/main/java/br/com/fullcustom/workshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package br.com.fullcustom.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.fullcustom.workshopmongo.domain.Post;
+import br.com.fullcustom.workshopmongo.repository.PostRepository;
+import br.com.fullcustom.workshopmongo.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repo;
+
+    public Post findById(String id) {
+        Optional<Post> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+}
